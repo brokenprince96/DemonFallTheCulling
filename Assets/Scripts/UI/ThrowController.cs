4e24e77cc990981d9f5789736c53618a48d88fd4
Isaiah Pone <147687746+brokenprince96@users.noreply.github.com>
2025-09-30 16:18:04 -0700

    Brand new code for throw

--- Assets/Scripts/UI/ThrowController.cs
+++ Assets/Scripts/UI/ThrowController.cs
@@ -3,17 +3,34 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Adding adjustments
 public class ThrowController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Button> throwButtons; // Assigned via Inspector
+    public GameObject playerGameObject; // Assigned via Inspector
+    Player player;
+    public TextMeshProUGUI throwsRemainingText;
+    int numThrowsRemaining = 3;
+
+    private void Awake()
     {
-        
+        player = playerGameObject.GetComponent<Player>();
+        throwsRemainingText.text = "Remaining moves: " + numThrowsRemaining;
+
+
+        for (int i = 0; i < throwButtons.Count; i++)
+        {
+            int index = i; // VERY IMPORTANT: capture index locally!
+
+            string buttonName = throwButtons[i].gameObject.name;
+
+            throwButtons[i].onClick.AddListener(() => OnActionClicked(buttonName));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnActionClicked(string buttonName)
     {
-        
+        throwsRemainingText.text = "Remaining moves: " + --numThrowsRemaining;
+        player.Throw(buttonName);
     }
 }
