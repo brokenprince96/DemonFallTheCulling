ffbdf2a31b1233b0ea58312c24d8e0a9ba8197d4
Isaiah Pone <147687746+brokenprince96@users.noreply.github.com>
2025-09-30 16:44:41 -0700

    Updated Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,14 +17,35 @@ public class Player : MonoBehaviour
             animator.SetTrigger(attack);
     }
 
+    // new Code to be added for throw 
+    public void Throw(string throws)
+    {
+        if(fightManager.PlayerCanAttack())
+            animator.SetTrigger(throws);
+    }
+
     public virtual void InflictDamage(float damageAmount)
     {
         enemy.TakeDamage(damageAmount);
     }
 
+    // New code added for more damage
+    public virtual void InflictMoreDamage(float damageAmount = 0.2f)
+    {
+        enemy.TakeMoreDamage(damageAmount);
+    }
+
     public virtual void TakeDamage(float damageAmount)
     {
         animator.SetTrigger("damage");
+        health -= damageAmount;
+        healthSlider.value = health;
+    }
+
+    public virtual void TakeMoreDamage(float damageAmount = 0.2f)
+    {
+
+        animator.SetTrigger("extra damage");
         health -= damageAmount;
         healthSlider.value = health;
     }
